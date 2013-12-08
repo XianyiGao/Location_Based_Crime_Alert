1fbe7c4cd1696c555a4f08131228c6558ee6f4f6
XianyiGao <xianyigmsn@hotmail.com>
2013-12-07 21:57:56 -0500

    versioon
    dfg

--- src/com/example/location_based_crime_alert/MainActivity.java
+++ src/com/example/location_based_crime_alert/MainActivity.java
@@ -1,22 +1,48 @@
 package com.example.location_based_crime_alert;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.GestureOverlayView.OnGesturePerformedListener;
+import android.gesture.Prediction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnGesturePerformedListener {
+  private GestureLibrary gestureLib;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+  
+/** Called when the activity is first created. */
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GestureOverlayView gestureOverlayView = new GestureOverlayView(this);
+    View inflate = getLayoutInflater().inflate(R.layout.activity_main, null);
+    gestureOverlayView.addView(inflate);
+    gestureOverlayView.addOnGesturePerformedListener(this);
+    gestureLib = GestureLibraries.fromRawResource(this, R.raw.gestures);
+    if (!gestureLib.load()) {
+      finish();
+    }
+    setContentView(gestureOverlayView);
+  }
 
-}
+  @Override
+  public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+    ArrayList<Prediction> predictions = gestureLib.recognize(gesture);
+    boolean flag=true;
+    for (Prediction prediction : predictions) {
+      if ((prediction.score > 1.0)&&(flag)) {
+        Toast.makeText(this, prediction.name, Toast.LENGTH_SHORT)
+            .show();
+        flag=false;
+      }
+    }
+  }
+} 
